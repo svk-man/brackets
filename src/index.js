e216430af773a3590d2a79b2129f3ed4b4f3ea77
Sergey Kravchenko <kravchenko.sgv@gmail.com>
2021-03-15 13:15:55 +0300

    refactor: implement check sequence brackets function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  let isPossible = true;
+  while (isPossible) {
+    isPossible = false;
+    for (let config of bracketsConfig) {
+      if (str.indexOf(config[0] + config[1]) != -1) {
+        str = str.replace(config[0] + config[1], '');
+        isPossible = true;
+      }
+    }
+  }
+
+  return (str.length ? false : true);
 }
